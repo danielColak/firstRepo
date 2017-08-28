6bd6e49d00993a55269d3feaee2a4803309a8cf2
Daniel Colak <daniel.colak@gmail.com>
2017-08-29 00:23:25 +0200

    dsdsdsds

--- real_names/new_1/new_98/real_names.py
+++ real_names/new_1/new_98/real_names.py
@@ -1,9 +1,11 @@
 class Welcome:
     """
     The class Welcome contains all real names of buttons, fields and hyperlinks from Welcome Screen. With Welcome.nameOfGetMethod() we call them.
+	d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
+	d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
     """
     def __init__(self):
-        self.welcomeWindow = "{caption='' isvisible='modifikacije na grani' type='org.eclipse.swt.widgets.Shell'}"
+        self.welcomeWindow = "{caption='' isvisible='d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\' type='org.eclipse.swt.widgets.Shell'}"
         self.devicePanelProjectHyperlink = "{caption='Divajs' id='devicePanelWizardLink' styletype='hyperlink' visible='true' window=" + self.welcomeWindow + "}"
         self.systemPanelProjectHyperlink = "{caption='System' id='systemPanelWizardLink' styletype='hyperlink' visible='true' window=" + self.welcomeWindow + "}"
         self.libraryProjectHyperlink = "{caption='Library Project' id='libraryPanelWizardLink' styletype='hyperlink' visible='true' window=" + self.welcomeWindow + "}"
@@ -25,3 +27,10 @@ class Welcome:
         self.recentProjectsListView = "{id='recentProjectsListView' styletype='list-view' visible='true' window=" + self.welcomeWindow + "}"
         self.welcomeCheckBox = "{caption='Always show at startup' id='alwaysShowOnStartupCheckbox' styletype='check-box white-check-box' visible='true' window=" + self.welcomeWindow + "}"
         self.recentProject = "{container=" + self.recentProjectsListView + " styletype='cell indexed-cell list-cell' visible='true'}"
+
+		d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
+		d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
+		d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
+		d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
+		d:\IT\RT-RK\GIT\gitHubRepo\real_names\new_1\new_98\
+		
