9e51b3ec8fab7a5c0588a779f26d1e801970d21c
Daniel Colak <daniel.colak@gmail.com>
2017-08-29 01:37:03 +0200

    new file structure

---
+++ real_names/new_1/1212/12121212/23232323/dan_2_zadatak_palindrom.py
@@ -0,0 +1,17 @@
+#samo jedan string:
+
+def palindrom(word):
+    return word == word[::-1]
+
+#ako je u pitanju recenica:
+
+from collections import deque
+def palindrom_recenica(word):
+    dq = deque(word)
+    while dq.__len__()!= 0:
+        if dq.pop() != dq.popleft():
+            return False
+        else: return True
+
+print(palindrom_recenica('ana voli milovana'))
+print(palindrom("sale"))
